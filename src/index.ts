1cf732fccdc652ad1a9a2b8d7a6d7f7767f2a42a
Daniel Imal <mrdanimal@gmail.com>
2022-06-13 14:44:20 -0700

    rename

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,20 @@ import styles from './styles.scss';
 window.addEventListener('load', async () => {
   document.body.className = styles.body;
 
-  const imagesToInvert = document.querySelectorAll('#transform tr img');
+  const imagesToTransform = document.querySelectorAll('#transform tr img');
 
-  imagesToInvert.forEach((row: Element, i: number) => {
+  imagesToTransform.forEach((row: Element, i: number) => {
     const imageData = makeImageDataFromImgElement(row as HTMLImageElement);
-    const transformedImage = invertImage(imageData);
+
+    const invertedImage = invertImage(imageData);
 
     const canvas: HTMLCanvasElement | null = document.querySelector(`#transform tr:nth-child(${i + 2}) canvas.invert`);
 
     if (canvas) {
-      canvas.width = transformedImage.width;
-      canvas.height = transformedImage.height;
+      canvas.width = invertedImage.width;
+      canvas.height = invertedImage.height;
 
-      canvas.getContext('2d')?.putImageData(transformedImage, 0, 0);
+      canvas.getContext('2d')?.putImageData(invertedImage, 0, 0);
     }
   });
 });
