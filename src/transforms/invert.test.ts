2750c44d6e033323e8056e547a4fa8e8a724e41d
Daniel Imal <mrdanimal@gmail.com>
2022-06-27 16:54:41 -0700

    flesh out the ImageData test

--- src/transforms/invert.test.ts
+++ src/transforms/invert.test.ts
@@ -33,9 +33,40 @@ describe('invertImageDataArray', () => {
   });
 });
 
-const arbitraryImageData: fc.Arbitrary<ImageData> = fc
-  .uint8ClampedArray()
-  .filter((data) => data.length % 4 === 0)
-  .map((data) => {
-    return new ImageData(data, 100, 100);
+function buildArbitraryImageData() {
+  const arbitraryImageData: fc.Arbitrary<ImageData> = fc
+    .uint8ClampedArray({ minLength: 4 })
+    .filter((data) => data.length % 4 === 0)
+    .map((data) => {
+      return new ImageData(data, 100, 100);
+    });
+
+  return arbitraryImageData;
+}
+
+describe('invert', () => {
+  describe('properties', () => {
+    it('should not equal the original input', () => {
+      fc.assert(
+        fc.property(buildArbitraryImageData(), (array) => {
+          const result = invert(array);
+          expect(result).not.toEqual(array);
+        })
+      );
+    });
+
+    it('should equal the original input when called on the output a second time', () => {
+      fc.assert(
+        fc.property(
+          fc.uint8ClampedArray({
+            minLength: 4,
+          }),
+          (array) => {
+            const result = invertImageDataArray(invertImageDataArray(array));
+            expect(result).toEqual(array);
+          }
+        )
+      );
+    });
   });
+});
