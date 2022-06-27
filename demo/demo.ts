367673a836fa6fd95a8a22fcdf2a86b24c9c0712
Daniel Imal <mrdanimal@gmail.com>
2022-06-27 15:46:09 -0700

    add comments to demo page

--- demo/demo.ts
+++ demo/demo.ts
@@ -4,6 +4,12 @@ import { invert, pixelate } from '../src/transforms';
 
 import styles from './styles.scss';
 
+/*
+ * Quick and dirty page for demoing image transforms in a browser. This probably
+ * won't scale as lots of transforms are added, but it's good enough for now. As
+ * new transforms are added, look for a more scalable way of demoing them.
+ */
+
 window.addEventListener('load', async () => {
   document.body.className = styles.body;
 
@@ -24,6 +30,9 @@ window.addEventListener('load', async () => {
   });
 });
 
+// Note: This is dependent on having a table with id `transform` with columns
+// for each transform type and canvas elements in each column with the class
+// name of the transform type
 function renderImageToTable(i: number, imageData: ImageData, canvasClassName: string) {
   const canvas: HTMLCanvasElement | null = document.querySelector(
     `#transform tr:nth-child(${i + 2}) canvas.${canvasClassName}`
@@ -34,5 +43,7 @@ function renderImageToTable(i: number, imageData: ImageData, canvasClassName: st
     canvas.height = imageData.height;
 
     canvas.getContext('2d')?.putImageData(imageData, 0, 0);
+  } else {
+    throw new Error("The canvas wasn't found! Is the classname correct? Did the structure of the table DOM change?");
   }
 }
