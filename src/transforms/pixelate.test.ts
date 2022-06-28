a53ea252d5c57b238002dd44e8f0eceaf0258947
Daniel Imal <mrdanimal@gmail.com>
2022-06-28 08:37:13 -0700

    Revert "remove test"
    This reverts commit f5b126da9eddb424efb23d1ea27086695fd841a6.

--- src/transforms/pixelate.test.ts
+++ src/transforms/pixelate.test.ts
@@ -49,4 +49,40 @@ describe('pixelate', () => {
       );
     });
   });
+
+  test('each pixel in each pixelated area of the image should be the same', () => {
+    fc.assert(
+      fc.property(buildArbitraryImageData(), (imageData) => {
+        const gridSize = 10;
+        const result = pixelate(imageData, 3);
+        const { width, height } = imageData;
+        const gridAreaColors: Record<string, IPixel> = {};
+
+        for (let i = 0; i < imageData.data.length; i = i + 4) {
+          const x = ((i % (width * 4)) / (width * 4)) * width;
+          const y = (Math.ceil(i / (height * 4)) / height) * height;
+
+          const pixel: IPixel = [result.data[i], result.data[i + 1], result.data[i + 2], result.data[i + 3]];
+
+          const key = `${Math.floor(x / gridSize)},${Math.floor(y / gridSize)}`;
+
+          if (!gridAreaColors[key]) {
+            gridAreaColors[key] = pixel;
+          } else {
+            if (
+              pixel[0] !== gridAreaColors[key][0] ||
+              pixel[1] !== gridAreaColors[key][1] ||
+              pixel[2] !== gridAreaColors[key][2] ||
+              pixel[3] !== gridAreaColors[key][3]
+            ) {
+              console.log({ pixel, expected: gridAreaColors[key] });
+            }
+
+            expect(pixel).toEqual(gridAreaColors[key]);
+          }
+        }
+      }),
+      { verbose: false }
+    );
+  });
 });
