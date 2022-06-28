694d780e0f1523e6b1774b5d386d19ee57ae46c6
Daniel Imal <mrdanimal@gmail.com>
2022-06-27 20:07:17 -0700

    first test

--- src/transforms/pixelate.test.ts
+++ src/transforms/pixelate.test.ts
@@ -1,10 +1,23 @@
 import fc from 'fast-check';
+import { MockImageData, buildArbitraryImageData } from '../test-helpers';
 
 import { pixelate } from './pixelate';
 
+// @ts-ignore. Good enough for tests
+global.ImageData = MockImageData;
+
 describe('pixelate', () => {
   describe('properties', () => {
-    test('the output should not equal the input ', () => {});
+    test('the output should not equal the input ', () => {
+      fc.assert(
+        fc.property(buildArbitraryImageData(), (imageData) => {
+          const result = pixelate(imageData);
+
+          expect(result).not.toEqual(imageData);
+        })
+      );
+    });
+
     test('the average color value of all pixels in the output should equal the average color value of all pixels in the input', () => {});
   });
 });
