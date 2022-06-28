4c8cc09870570f1605d2438603e215ff9cf5c86a
Daniel Imal <mrdanimal@gmail.com>
2022-06-28 07:08:19 -0700

    test that the average of all pixels is the same

--- src/transforms/pixelate.test.ts
+++ src/transforms/pixelate.test.ts
@@ -6,6 +6,24 @@ import { pixelate } from './pixelate';
 // @ts-ignore. Good enough for tests
 global.ImageData = MockImageData;
 
+function getAveragePixelValues(imageDataArray: Uint8ClampedArray) {
+  const sums = [0, 0, 0, 0];
+
+  for (let i = 0; i < imageDataArray.length; i = i + 4) {
+    sums[i] = imageDataArray[i];
+    sums[i + 1] = imageDataArray[i + 1];
+    sums[i + 2] = imageDataArray[i + 2];
+    sums[i + 3] = imageDataArray[i + 3];
+  }
+
+  return [
+    Math.floor(sums[0] / imageDataArray.length),
+    Math.floor(sums[1] / imageDataArray.length),
+    Math.floor(sums[2] / imageDataArray.length),
+    Math.floor(sums[3] / imageDataArray.length),
+  ];
+}
+
 describe('pixelate', () => {
   describe('properties', () => {
     test('the output should not equal the input ', () => {
@@ -22,13 +40,8 @@ describe('pixelate', () => {
       fc.assert(
         fc.property(buildArbitraryImageData(), (imageData) => {
           const result = pixelate(imageData);
-          console.log('imageData', imageData.data);
-          console.log('result', imageData.data);
-
-          const averageInput = imageData.data.reduce((acc, item) => acc + item) / imageData.data.length;
-          const averageOutput = imageData.data.reduce((acc, item) => acc + item) / result.data.length;
-
-          console.log({ averageInput, averageOutput });
+          const averageInput = getAveragePixelValues(imageData.data);
+          const averageOutput = getAveragePixelValues(result.data);
 
           expect(averageOutput).toEqual(averageInput);
         })
