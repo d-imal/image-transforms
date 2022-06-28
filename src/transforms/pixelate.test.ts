14ade69b1217d3ad7995e82a46cf5ad88f58bf2f
Daniel Imal <mrdanimal@gmail.com>
2022-06-28 07:50:24 -0700

    closer

--- src/transforms/pixelate.test.ts
+++ src/transforms/pixelate.test.ts
@@ -53,18 +53,22 @@ describe('pixelate', () => {
   test('each pixel on each pixelated area of the image should be the same', () => {
     fc.assert(
       fc.property(buildArbitraryImageData(), (imageData) => {
-        const result = pixelate(imageData);
+        const gridSize = 10;
+        const result = pixelate(imageData, 5);
         const { width, height } = imageData;
         const gridAreaColors: Record<string, IPixel> = {};
 
         for (let i = 0; i < imageData.data.length; i = i + 4) {
-          const x = (i % (width * 4)) / (width * 4);
-          const y = Math.floor(i / (height * 4)) / height;
+          const x = ((i % (width * 4)) / (width * 4)) * width;
+          const y = (Math.floor(i / (height * 4)) / height) * height;
+
           const pixel: IPixel = [result.data[i], result.data[i + 1], result.data[i + 2], result.data[i + 3]];
 
-          const key = `${x},${y}`; // Wrong
+          const key = `${Math.floor(x / gridSize)},${Math.floor(y / gridSize)}`;
 
           if (!gridAreaColors[key]) {
+            // console.log(key);
+
             gridAreaColors[key] = pixel;
           } else {
             expect(pixel).toEqual(gridAreaColors[key]);
