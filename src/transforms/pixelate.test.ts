99bab891153ddbe1cfcfcae1cd3ecf4007866109
Daniel Imal <mrdanimal@gmail.com>
2022-06-28 08:22:38 -0700

    debugging

--- src/transforms/pixelate.test.ts
+++ src/transforms/pixelate.test.ts
@@ -54,27 +54,35 @@ describe('pixelate', () => {
     fc.assert(
       fc.property(buildArbitraryImageData(), (imageData) => {
         const gridSize = 10;
-        const result = pixelate(imageData, 5);
+        const result = pixelate(imageData, 3);
         const { width, height } = imageData;
         const gridAreaColors: Record<string, IPixel> = {};
 
         for (let i = 0; i < imageData.data.length; i = i + 4) {
           const x = ((i % (width * 4)) / (width * 4)) * width;
-          const y = (Math.floor(i / (height * 4)) / height) * height;
+          const y = (Math.ceil(i / (height * 4)) / height) * height;
 
           const pixel: IPixel = [result.data[i], result.data[i + 1], result.data[i + 2], result.data[i + 3]];
 
           const key = `${Math.floor(x / gridSize)},${Math.floor(y / gridSize)}`;
 
           if (!gridAreaColors[key]) {
-            // console.log(key);
-
             gridAreaColors[key] = pixel;
           } else {
+            if (
+              pixel[0] !== gridAreaColors[key][0] ||
+              pixel[1] !== gridAreaColors[key][1] ||
+              pixel[2] !== gridAreaColors[key][2] ||
+              pixel[3] !== gridAreaColors[key][3]
+            ) {
+              console.log({ pixel, expected: gridAreaColors[key] });
+            }
+
             expect(pixel).toEqual(gridAreaColors[key]);
           }
         }
-      })
+      }),
+      { verbose: false }
     );
   });
 });
