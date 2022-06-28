61e0a13a13fed72c7eeba8cc148bb104336c0749
Daniel Imal <mrdanimal@gmail.com>
2022-06-27 19:59:54 -0700

    stub pixelate test

---
+++ src/transforms/pixelate.test.ts
@@ -0,0 +1,10 @@
+import fc from 'fast-check';
+
+import { pixelate } from './pixelate';
+
+describe('pixelate', () => {
+  describe('properties', () => {
+    test('the output should not equal the input ', () => {});
+    test('the average color value of all pixels in the output should equal the average color value of all pixels in the input', () => {});
+  });
+});
