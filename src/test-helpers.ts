419982753e2e4374e25d2f88290baef36e30b72c
Daniel Imal <mrdanimal@gmail.com>
2022-06-27 20:05:10 -0700

    move test helpers out to separate file

---
+++ src/test-helpers.ts
@@ -0,0 +1,21 @@
+import fc from 'fast-check';
+
+export class MockImageData {
+  public data: Uint8ClampedArray;
+  public colorSpace: PredefinedColorSpace = 'srgb';
+  public width = 100;
+  public height = 100;
+
+  constructor(data: Uint8ClampedArray) {
+    this.data = data;
+  }
+}
+
+export function buildArbitraryImageData() {
+  const arbitraryImageData: fc.Arbitrary<MockImageData> = fc
+    .uint8ClampedArray({ minLength: 4 })
+    .filter((data) => data.length % 4 === 0)
+    .map((data) => new MockImageData(data));
+
+  return arbitraryImageData;
+}
--- src/transforms/invert.test.ts
+++ src/transforms/invert.test.ts
@@ -1,28 +1,9 @@
 import fc from 'fast-check';
+import { MockImageData, buildArbitraryImageData } from '../test-helpers';
 import { invert } from './invert';
-
-class MockImageData {
-  public data: Uint8ClampedArray;
-  public colorSpace: PredefinedColorSpace = 'srgb';
-  public width = 100;
-  public height = 100;
-
-  constructor(data: Uint8ClampedArray) {
-    this.data = data;
-  }
-}
 
 // @ts-ignore. Good enough for tests
 global.ImageData = MockImageData;
-
-function buildArbitraryImageData() {
-  const arbitraryImageData: fc.Arbitrary<MockImageData> = fc
-    .uint8ClampedArray({ minLength: 4 })
-    .filter((data) => data.length % 4 === 0)
-    .map((data) => new MockImageData(data));
-
-  return arbitraryImageData;
-}
 
 describe('invert', () => {
   describe('properties', () => {
