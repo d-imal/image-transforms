3394ae7d580e4137ec5b28493fc8923425fc63fb
Daniel Imal <mrdanimal@gmail.com>
2022-06-27 17:29:37 -0700

    fix var name

--- src/transforms/invert.test.ts
+++ src/transforms/invert.test.ts
@@ -30,20 +30,19 @@ describe('invert', () => {
   describe('properties', () => {
     it('should not equal the original input', () => {
       fc.assert(
-        fc.property(buildArbitraryImageData(), (array) => {
-          const result = invert(array);
-          // console.log({ array, result });
+        fc.property(buildArbitraryImageData(), (imageData) => {
+          const result = invert(imageData);
 
-          expect(result).not.toEqual(array);
+          expect(result).not.toEqual(imageData);
         })
       );
     });
 
     it('should equal the original input when called on the output a second time', () => {
       fc.assert(
-        fc.property(buildArbitraryImageData(), (array) => {
-          const result = invert(invert(array));
-          expect(result).toEqual(array);
+        fc.property(buildArbitraryImageData(), (imageData) => {
+          const result = invert(invert(imageData));
+          expect(result).toEqual(imageData);
         })
       );
     });
